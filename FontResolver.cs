0cc505fb531083f5a83db245d0185b577fe075f5
Muharrem Erbay <muharremerbay@gmail.com>
2024-12-04 16:06:43 +0300

    fontresolver ayrı dosyaya alındı

---
+++ FontResolver.cs
@@ -0,0 +1,79 @@
+﻿using PdfSharp.Fonts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPdf
+{
+    public class FontResolver : IFontResolver
+    {
+        public FontResolverInfo? ResolveTypeface(string familyName, bool isBold, bool isItalic)
+        {
+            switch (familyName)
+            {
+                case "Bahnschrift":
+                case "Oswald-Light":
+                case "Oswald-Regular":
+                case "Oswald-Bold":
+                    return new FontResolverInfo(familyName);
+            }
+
+            // We pass all other font requests to the default handler.
+            // When running on a web server without sufficient permission, you can return a default font at this stage.
+            return PlatformFontResolver.ResolveTypeface(familyName, isBold, isItalic);
+        }
+
+        static byte[] LoadFontData(string name)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            // Test code to find the names of embedded fonts
+            var ourResources = assembly.GetManifestResourceNames();
+
+            using (Stream? stream = assembly.GetManifestResourceStream(name))
+            {
+                if (stream == null)
+                    throw new ArgumentException("No resource with name " + name);
+
+                int count = (int)stream.Length;
+                byte[] data = new byte[count];
+                stream.Read(data, 0, count);
+                return data;
+            }
+        }
+
+        /// <summary>
+        /// Return the font data for the fonts.
+        /// </summary>
+        public byte[]? GetFont(string fontName)
+        {
+            try
+            {
+                return LoadFontData($"ConsoleAppPdf.Resources.{fontName}.ttf");
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        internal static FontResolver? OurGlobalFontResolver = null;
+
+        /// <summary>
+        /// Ensure the font resolver is only applied once (or an exception is thrown)
+        /// </summary>
+        internal static void Apply()
+        {
+            if (OurGlobalFontResolver == null || GlobalFontSettings.FontResolver == null)
+            {
+                if (OurGlobalFontResolver == null)
+                    OurGlobalFontResolver = new FontResolver();
+
+                GlobalFontSettings.FontResolver = OurGlobalFontResolver;
+            }
+        }
+    }
+}
